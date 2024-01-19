6660b9ec3b24fbd3e833e6db4ef1261281b857d2
Esteban Cortina <estebannsebastian@gmail.com>
2024-01-19 10:12:45 -0600

    Add mysql2

--- index.mjs
+++ index.mjs
@@ -1,7 +1,6 @@
 import express from "express";
 import morgan from "morgan";
 import { PORT } from "#config/env.mjs";
-import pool from "#config/mysql.mjs";
 
 const app = express();
 app.use(morgan("common"));
